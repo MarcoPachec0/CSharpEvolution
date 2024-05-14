4d18568367d648ce260e0c097763d992d708c89f
Marco Pacheco <marcoa829@gmail.com>
2024-05-14 09:59:50 -0300

    Add files via upload

---
+++ programs/DisplayLiteralAndVariableValues.cs
@@ -0,0 +1,5 @@
+string name = "bob";
+int number = 3;
+float number2 = 34.4f;
+
+Console.WriteLine("Hello, " + name + "! You Have " + number + " messages in your inbox. The temperatura is " + number2 + " celsius.");
